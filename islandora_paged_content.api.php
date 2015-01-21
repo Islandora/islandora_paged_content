32841464e984b4468d1e9c79ffd735b5b457282f
Jordan Dukart <jordan@discoverygarden.ca>
2015-01-21 16:02:11 -0400

    Update hook structure, small fixes.

--- islandora_paged_content.api.php
+++ islandora_paged_content.api.php
@@ -7,9 +7,15 @@
 
 /**
  * Identifies content models whose children are paged content.
+ *
+ * @return array
+ *   An array mapping the parent paged content model to an array of its child
+ *   pages.
  */
 function hook_islandora_paged_content_content_model_registry() {
   return array(
-    'somecmodel',
+    'somecmodel' => array(
+      'somepagecmodel',
+    ),
   );
 }
